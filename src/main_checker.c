781250dc240f4cb34049173f776100ecae3313c2
kodgirl <startsava@gmail.com>
2020-02-15 16:35:14 +0300

    without leaks. norminette 1 error

--- src/main_checker.c
+++ src/main_checker.c
@@ -6,7 +6,7 @@
 /*   By: bjasper <bjasper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 13:11:05 by bjasper           #+#    #+#             */
-/*   Updated: 2020/02/15 16:17:09 by bjasper          ###   ########.fr       */
+/*   Updated: 2020/02/15 16:28:00 by bjasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ int		main(int ac, char **av)
 
 	ft_bzero(&stack, sizeof(t_stack));
 	if (ac == 1 || ft_make_stack(ac - 1, av, &stack) == 0)
-	{
-		// del_stacks(&stack);
 		return (0);
-	}
 	make_instr(&i_list);
 	while ((size = get_next_line(0, &line)) > 0)
 	{
 		if (read_instruct(line, &stack, &i_list) == 0)
 		{
 			del_stacks(&stack);
+			free(line);
 			return (0);
 		}
 		free(line);
